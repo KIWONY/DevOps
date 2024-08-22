5cd7411c068c4533635c34aad78c4c223bac9b83
KIWONY <kien1004@naver.com>
2024-08-22 19:08:37 +0900

    feat: grpc server, client 도커 이미지, 정상 응답 확인 완료

--- grpcdockerimage/image_grpc_client/main.py
+++ grpcdockerimage/image_grpc_client/main.py
@@ -1,19 +1,23 @@
-import json, uvicorn
+import json, uvicorn, logging
 
 import grpc
 from fastapi import FastAPI
 import love_pb2, love_pb2_grpc
 app = FastAPI()
 
-channel = grpc.insecure_channel('0.0.0.0:6000', options=(('grpc.enable_http_proxy', 0),))
-stub = love_pb2_grpc.LoveGrpcStub(channel)
+channel = grpc.insecure_channel('grpc_server:6000', options=(('grpc.enable_http_proxy', 0),))
+love_stub = love_pb2_grpc.LoveGrpcStub(channel)
 
 @app.post("/love")
 def communicate_love(speaker: str):
-    bytes_data = bytes(json.dumps(speaker), encoding='UTF-8')
-    response = stub.Love(love_pb2.Input(approach=bytes_data))
+    bytes_data = speaker.encode('UTF-8')
+    print(bytes_data)
+    request = love_pb2.Input(approach=bytes_data)
+    response = love_stub.Love(request)
+    response = response.acceptance.decode('UTF-8')
     return response
 
 
 if __name__ == "__main__":
+    logging.info("start")
     uvicorn.run("main:app", host="0.0.0.0", port=8010, reload=True)
--- grpcdockerimage/image_grpc_server/server.py
+++ grpcdockerimage/image_grpc_server/server.py
@@ -3,22 +3,31 @@ from concurrent import futures
 import json
 
 class LoveGRPC(love_pb2_grpc.LoveGrpcServicer):
-
     def Love(self, request, context):
 
         """
-        request: 클라이언트로부터 받은 요청
-
+        request: 클라이언트로부터 받은 요청 
         context: RPC 상황 정보 포함
         """
-        json_data = json.loads(request.decode('UTF-8'))
-        return love_pb2.Output(acceptance=f"grpc love, {json_data} and pong")
-
-
+        try:
+            approach = request.approach.decode('utf-8')
+            reply = f"grpc love, {approach} and pong"
+            reply_msg = bytes(reply, encoding="UTF-8")
+            accept = love_pb2.Output(acceptance=reply_msg)
+            return accept
+        
+        
+        except Exception as e:
+            # 'errors'를 올바르게 정의하고 사용
+            context.set_code(grpc.StatusCode.ABORTED)
+            print(e)
+            return str(e)
+        
 def server():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))        # gRPC 서버를 생성, ThreadPoolExecutor: 서버가 동시에 처리할 수 있는 요청의 최대 수
     love_pb2_grpc.add_LoveGrpcServicer_to_server(LoveGRPC(), server)
     server.add_insecure_port('[::]:6000')                                  # [::] : 모든 IP 주소에서의 접속을 허용
+    server.add_insecure_port('grpc_server:6000')
     server.start()
     print("server starts...")
     server.wait_for_termination()                                           #
